a7fb6ad580b3d4528791b57481a317b40cfa302d
david-develop <924@holbertonschool.com>
2019-11-09 16:39:50 -0500

    [Update] Console method update

--- console.py
+++ console.py
@@ -68,6 +68,55 @@ class HBNBCommand(cmd.Cmd):
                     del all_objs[key]
                     storage.save()
 
+    def do_all(self, line):
+        """
+        Prints all string representation of all instances based or not on the
+        class name. Ex: $ all BaseModel or $ all
+        """
+        if not line:
+            all_objs = storage.all()
+            list_obj = []
+            for objs in all_objs:
+                list_obj.append(str(all_objs[objs]))
+            print(list_obj)
+        else:
+            list_arg = line.split()
+            for arg in list_arg:
+                if arg not in self.com_list:
+                    print("** class doesn't exist **")
+                else:
+                    all_objs = storage.all()
+                    list_obj = []
+                    for key_obj in all_objs:
+                        k = key_obj.split(".")
+                        if k[0] == arg:
+                            list_obj.append(str(all_objs[key_obj]))
+                    print(list_obj)
+
+    def do_update(self, line):
+        """
+        Updates an instance based on the class name and id by adding or
+        updating attribute (save the change into the JSON file)
+        """
+        list_arg = line.split()
+        if len(list_arg) == 0:
+            print("** class name missing **")
+        elif list_arg[0] not in self.com_list:
+            print("** class doesn't exist **")
+        elif len(list_arg) < 2:
+            print("** instance id missing **")
+        else:
+            key = list_arg[0] + "." + list_arg[1]
+            all_objs = storage.all()
+            if key not in all_objs:
+                print("** no instance found **")
+            elif len(list_arg) < 3:
+                print("** attribute name missing **")
+            elif len(list_arg) < 4:
+                print("** value missing **")
+
+
+
     def do_quit(self, arg):
         """stop the command line interpreter"""
         return True
@@ -78,7 +127,7 @@ class HBNBCommand(cmd.Cmd):
         return True
 
     def emptyline(self):
-         pass
+        pass
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
