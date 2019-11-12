579beab17be8bdad96dff4ad62bae8d3523fd4c0
Sergio Zamudio <szamu5@hotmail.com>
2019-11-12 12:12:43 -0500

    [FIX]: check pep8 style New file: HBnB.png and console.png Modified: console.py

--- console.py
+++ console.py
@@ -11,13 +11,13 @@ from models.city import City
 from models.amenity import Amenity
 from models.review import Review
 
+
 class HBNBCommand(cmd.Cmd):
     """class HBNBCommand command interpreter"""
     prompt = '(hbnb) '
-    com_list = {"BaseModel" : BaseModel, "User" : User, "Place": Place,
+    com_list = {"BaseModel": BaseModel, "User": User, "Place": Place,
                 "State": State, "City": City, "Amenity": Amenity,
                 "Review": Review}
-
 
     def do_create(self, arg):
         """
