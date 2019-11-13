ad54b1a5d54188a9a8a4221c7f237bfa11a126e9
david-develop <924@holbertonschool.com>
2019-11-13 10:38:25 -0500

    [Fix] User Class tests

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -8,6 +8,7 @@ from models.base_model import BaseModel
 from models.user import User
 from models import storage
 import os
+import datetime
 
 
 class TestUserClass(unittest.TestCase):
@@ -20,19 +21,32 @@ class TestUserClass(unittest.TestCase):
     my_user.password = "root"
 
     def test_instance(self):
+        """test instance"""
         self.assertTrue(isinstance(self.my_user, User))
 
     def test_inheritance(self):
+        """test inherit from BaseModel"""
         self.assertTrue(issubclass(User, BaseModel))
         self.assertTrue(issubclass(type(self.my_user), BaseModel))
 
+    def test_attr(self):
+        """test attributes"""
+        self.assertTrue(hasattr(self.my_user, 'first_name'))
+        self.assertTrue(hasattr(self.my_user, 'last_name'))
+        self.assertTrue(hasattr(self.my_user, 'email'))
+        self.assertTrue(hasattr(self.my_user, 'password'))
+
     def test_types(self):
+        """test attributes types"""
         self.assertTrue(type(self.my_user.email) is str)
         self.assertTrue(type(self.my_user.password) is str)
         self.assertTrue(type(self.my_user.first_name) is str)
         self.assertTrue(type(self.my_user.last_name) is str)
+        self.assertIsInstance(self.my_user.created_at, datetime.datetime)
+        self.assertIsInstance(self.my_user.updated_at, datetime.datetime)
 
     def test_des_serialization(self):
+        """test seriarization and deserialization"""
         user_dict = self.my_user.to_dict()
         self.assertTrue(type(user_dict) is dict)
         if os.path.exists("file.json"):
